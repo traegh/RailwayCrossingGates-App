a64e269a32785de87644ed73e23e4112a6b4be83
Daniel Strach <strach.daniel.dev@gmail.com>
2023-07-18 11:11:12 +0200

    First part of a program. Selenium types `Gogolin` into the field available and continues, awaiting for further instructions to come.

---
+++ main.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Inicjalizacja przeglądarki
+driver = webdriver.Chrome()
+driver.get("https://bilkom.pl/stacje/tablica")
+
+# Wyszukanie pola wprowadzania frazy
+from_station_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "fromStation"))
+)
+
+# Wpisanie frazy "GOGOLIN"
+from_station_input.clear()
+from_station_input.send_keys("GOGOLIN")
+time.sleep(5)
+
+# Kliknięcie przycisku "POKAŻ TABLICE"
+search_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "search-btn"))
+)
+search_button.click()
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "result-element-id"))
+)
