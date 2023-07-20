1759df6753286a4e783bc0b9235f0392c7460790
Daniel Strach <strach.daniel.dev@gmail.com>
2023-07-20 09:36:47 +0200

    new code: made connection to database. saving each one of them records. made sure there probably won't be any duplicates (require testing).

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium import webdriver; from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import re
-import html
-import random
 from datetime import datetime, timedelta
-
+import time; import re; import html
+import random; import pymysql
 
 def add_delay(time_value, delay_value):
     time_format = "%H:%M"
@@ -17,33 +13,40 @@ def add_delay(time_value, delay_value):
     new_time_value = new_time_obj.strftime(time_format)
     return new_time_value
 
-
-driver = webdriver.Chrome()
-driver.get("https://bilkom.pl/stacje/tablica")
-from_station_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "fromStation"))
-)
-time.sleep(round(random.uniform(0.50, 2.10), 2))
-
-# Fill the field with "GOGOLIN"
-from_station_input.clear()
-from_station_input.send_keys("GOGOLIN")
-time.sleep(round(random.uniform(2.00, 4.99), 2))
-
-# Click the "SHOW TIMETABLE" button
-search_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, "search-btn"))
-)
-search_button.click()
-time.sleep(round(random.uniform(1.20, 3.99), 2))
-
-
 def extract_delay_value(text):
     match = re.search(r"\+(\d+)", text)
     if match:
         return match.group(1)
     return ""
 
+driver = webdriver.Chrome()
+driver.get("https://bilkom.pl/stacje/tablica")
+from_station_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "fromStation"))
+)
+
+# Setting basic anti-bot detection measures
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+# end of the settings
+
+time.sleep(round(random.uniform(0.50, 2.10), 2))    #anti-anti-bot
+driver.execute_script('window.scrollTo(0, 3)')      #anti-anti-bot
+
+# Fill the field with "GOGOLIN"
+wait = WebDriverWait(driver, 2)
+from_station_input.clear()
+from_station_input.send_keys("GOGOLIN")
+time.sleep(round(random.uniform(2.00, 4.99), 2))    #anti-anti-bot
+
+# Click the "POKAZ TABLICE" button
+search_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "search-btn"))
+); search_button.click()
+time.sleep(round(random.uniform(1.20, 3.99), 2))    #anti-anti-bot
 
 # Save times and ICC/R tags
 timetable = {}
@@ -63,11 +66,27 @@ for i in range(len(time_elements)):
             continue
 
     timetable[tag_value] = (time_value, delay_value)
-
 driver.quit()
 
-# Print the timetable
-# Print the timetable
+conn = pymysql.connect(
+    host="db4free.net",
+    user="crud_maker",
+    password="crud_maker",
+    database="crud_maker"
+); cursor = conn.cursor()
+
+# Dodawanie danych do tabeli w db
+for tag, time_value in timetable.items():
+    delay_value = time_value[1]
+    time_with_delays = add_delay(time_value[0], delay_value)
+    data_value = datetime.now().strftime("%d/%m/%y")
+
+    query = "INSERT INTO hours (data, train_tag, time) VALUES (%s, %s, %s) " \
+            "ON DUPLICATE KEY UPDATE time=%s"
+    cursor.execute(query, (data_value, tag, time_with_delays, time_with_delays))
+    conn.commit()
+cursor.close(); conn.close()
+
 for tag, time_value in timetable.items():
     delay_value = time_value[1]
     time_with_delays = add_delay(time_value[0], delay_value)
