dbfec204dbcef98d394e1cede1e0bfe86cdb0982
Daniel Strach <strach.daniel.dev@gmail.com>
2023-07-26 09:03:40 +0200

    new code: time-check if it's between 23-3 AM to prevent script from scraping when site's theoretically offline.

--- run_script.sh
+++ run_script.sh
@@ -2,18 +2,25 @@
 
 function kill {
     pkill -f "python3 /Users/mrarab/Documents/GitHub/RailwayCrossingGates-App/main.py"
-    echo "closed main.py "
+    echo "closed main.py"
 }
 
 trap kill SIGINT
 
 while true; do
-    echo "launching main.py... "
-    python3 /Users/mrarab/Documents/GitHub/RailwayCrossingGates-App/main.py
-    sleep 13
-    kill
-    x=$((140 + RANDOM % 10))
-    time=$(date +"%H:%M:%S")
-    echo "[$time] waiting till the next launch $x seconds. "
-    sleep $x
+    current_hour=$(date +"%H")
+
+    # while(time != (24-3 AM))
+    if [ "$current_hour" -ge 3 ] && [ "$current_hour" -lt 24 ]; then
+        echo "[G: $current_hour] launching main.py... "
+        python3 /Users/mrarab/Documents/GitHub/RailwayCrossingGates-App/main.py
+        sleep 10
+        kill
+        x=$((140 + RANDOM % 10))
+        time=$(date +"%H:%M:%S")
+        echo "[$time] awaiting for my work in $x seconds. . . "
+        sleep $x
+    else
+        echo "[G: $current_hour] Maintenance. Time between 24:00-3:00 detected, script will continue sometime later."
+    fi
 done
